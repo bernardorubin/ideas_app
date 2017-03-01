d8461c95c14d1052e1e0386c62702d4b6106ed15
bernulander <bernulander@gmail.com>
2017-03-01 09:28:14 -0800

    edit javascript syntax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,16 +14,20 @@
 //= require jquery_ujs
 //= require_tree .
 //= require bootstrap-sprockets
- $(document).ready(function(){
-  $( ".title" ).click(function() {
+
+$(document).ready(function(){
+
+  $(".title").click(function() {
     $(this).next('div').fadeToggle("description");
   });
-  $( ".lighttrigger" ).mouseenter(function() {
+  $(".lighttrigger").mouseenter(function() {
     $(".lightbulb").addClass("enter");
     $(".lightbulb").removeClass("leave");
   });
-  $( ".lighttrigger").mouseleave(function() {
+
+  $(".lighttrigger").mouseleave(function() {
     $(".lightbulb").removeClass("enter");
     $(".lightbulb").addClass("leave");
   });
+
 });
